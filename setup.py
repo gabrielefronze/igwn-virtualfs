f3aa3a7f23c0178755c86c83d552ddcdaf441730
Gabriele Fronzé <sucre.91@hotmail.it>
2021-10-12 13:40:08 +0200

    Improving setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from os import makedirs, path, mkdir, removedirs, symlink, remove
+from os import makedirs, path, mkdir, remove
+from typing import List
 import yaml
 import subprocess
 
@@ -6,19 +7,27 @@ basePath = path.dirname(__file__)
 framesConfigFileName = "framePaths.yml"
 
 
-def getLinkPathByExp(base, exp, channel, period, name):
+def linkPathByExp(base, exp, channel, period, name):
     return path.join(base, "exp", exp, channel, period, name)
 
 
-def getLinkPathByPeriod(base, exp, channel, period, name):
-    return path.join(base, "periods", period, exp, channel, name)\
+def linkPathByPeriod(base, exp, channel, period, name):
+    return path.join(base, "periods", period, exp, channel, name)
 
 
 def ln(source, dest):
     bashCommand = f"ln -s {source} {dest}"
-    print(bashCommand)
+    print(f"{source} -> {dest}")
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+
+
+def createLink(source, dest):
+    if not path.exists(path.dirname(dest)):
+        makedirs(path.dirname(dest))
+
+    remove(dest)
+    ln(source, dest)
 
 
 def initFrames():
@@ -30,29 +39,40 @@ def initFrames():
     with open(path.join(basePath, framesConfigFileName)) as framesConfigFile:
         config = yaml.load(framesConfigFile, Loader=yaml.FullLoader)
         for exp, channels in config.items():
-            # print(exp)
-            # print(channels)
             for channel in channels:
                 for channelName, periods in channel.items():
-                    # print(channelName)
                     for period in periods:
                         for periodName, links in period.items():
-                            # print(periodName)
-                            # print(links)
                             if links != "none":
-                                for link in links:
-                                    # print(link)
-                                    source, name = link.split(" -> ")
-                                    dest = getLinkPathByExp(framesBasePath, exp, channelName, periodName, name)
-                                    # print(dest)
-                                    if not path.exists(path.dirname(dest)):
-                                        makedirs(path.dirname(dest))
-                                    ln(source, dest)
-                                    dest = getLinkPathByPeriod(framesBasePath, exp, channelName, periodName, name)
-                                    # print(dest)
-                                    if not path.exists(path.dirname(dest)):
-                                        makedirs(path.dirname(dest))
-                                    ln(source, dest)
+                                if isinstance(links, List):
+                                    for link in links:
+                                        source, name = link.split(" -> ")
+                                        dest = linkPathByExp(framesBasePath,
+                                                             exp,
+                                                             channelName,
+                                                             periodName,
+                                                             name)
+                                        createLink(source, dest)
+                                        dest = linkPathByPeriod(framesBasePath,
+                                                                exp,
+                                                                channelName,
+                                                                periodName,
+                                                                name)
+                                        createLink(source, dest)
+                                elif isinstance(links, str):
+                                    source, name = links.split(" -> ")
+                                    dest = linkPathByExp(framesBasePath,
+                                                         exp,
+                                                         channelName,
+                                                         periodName,
+                                                         name)
+                                    createLink(source, dest)
+                                    dest = linkPathByPeriod(framesBasePath,
+                                                            exp,
+                                                            channelName,
+                                                            periodName,
+                                                            name)
+                                    createLink(source, dest)
     return
 
 
